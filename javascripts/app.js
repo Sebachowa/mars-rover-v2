01872f355d28bb461c7a9bd966dc39ef831b97b7
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-03-16 01:43:59 +0100

    iteration 2 finished

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,30 @@
 // Rover Object Goes Here
 // ======================
 
-var rober = {
-  direction = "N"
+var rover = {
+  direction: "W"
 }
 
 
 // ======================
 function turnLeft(rover){
+  console.log("The rover is now facing " + rover.direction);
   console.log("turnLeft was called!");
-  //First turns then one step forward
+  if (rover.direction === "N") {
+    rover.direction = "W"
+    console.log("The rover is now facing " + rover.direction);
+  } else if (rover.direction === "W") {
+    rover.direction = "S"
+    console.log("The rover is now facing " + rover.direction); 
+  } else if (rover.direction === "S") {
+    rover.direction = "E"
+    console.log("The rover is now facing " + rover.direction);
+  } else if (rover.direction === "E") {
+    rover.direction = "N"
+    console.log("The rover is now facing " + rover.direction);
+  } else {
+    alert("Something when worng bruh!!!")
+  };
 }
 
 function turnRight(rover){
@@ -21,3 +36,5 @@ function moveForward(rover){
   console.log("moveForward was called");
   //First turns then one step forward
 }
+
+turnLeft(rover);
