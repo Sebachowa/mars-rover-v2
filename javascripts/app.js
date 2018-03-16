3e991061761f342344cfec04972518bd46a18843
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-03-16 01:21:57 +0100

    iteration 1 finished

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,23 @@
 // Rover Object Goes Here
 // ======================
 
+var rober = {
+  direction = "N"
+}
+
+
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  //First turns then one step forward
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  //First turns then one step forward
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  console.log("moveForward was called");
+  //First turns then one step forward
 }
